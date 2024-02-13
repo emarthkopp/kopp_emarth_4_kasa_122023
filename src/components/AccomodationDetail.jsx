368cab49d1a81ee8b901bdd0973c8d93cd283a47
emarthkopp <emarth@outlook.com>
2024-02-13 20:17:40 +0100

    scss responsive terminé

--- src/components/AccomodationDetail.jsx
+++ src/components/AccomodationDetail.jsx
@@ -1,33 +1,50 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import accomodationData from '../data/accomodationData';
-import StarRating from './StarRating';
-import Gallery from './Gallery'; 
-import Description from './Description';
-import Equipments from './Equipments';
-import '../styles/AccomodationDetail.scss';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import accomodationData from "../data/accomodationData";
+import StarRating from "./StarRating";
+import Gallery from "./Gallery";
+import Description from "./Description";
+import Equipments from "./Equipments";
+import "../styles/AccomodationDetail.scss";
 
 function AccomodationDetail() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const { id } = useParams();
-  const accommodation = accomodationData.find((item) => item.id.toString() === id);
+  const accommodation = accomodationData.find(
+    (item) => item.id.toString() === id
+  );
 
   if (!accommodation) {
     return <div>Hébergement non trouvé</div>;
   }
 
-  const { title, pictures, host, rating, location, tags, description, equipments } = accommodation;
+  const {
+    title,
+    pictures,
+    host,
+    rating,
+    location,
+    tags,
+    description,
+    equipments,
+  } = accommodation;
 
-  const hostFullName = host && host.name ? host.name : "Nom de l'hôte non disponible";
+  const hostFullName =
+    host && host.name ? host.name : "Nom de l'hôte non disponible";
   const [hostFirstName, hostLastName] = hostFullName.split(" ");
-  const hostPicture = host && host.picture ? host.picture : "Image de l'hôte non disponible";
+  const hostPicture =
+    host && host.picture ? host.picture : "Image de l'hôte non disponible";
 
   const handleClickPrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : pictures.length - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : pictures.length - 1
+    );
   };
 
   const handleClickNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex < pictures.length - 1 ? prevIndex + 1 : 0));
+    setCurrentIndex((prevIndex) =>
+      prevIndex < pictures.length - 1 ? prevIndex + 1 : 0
+    );
   };
 
   return (
@@ -40,33 +57,42 @@ function AccomodationDetail() {
           handleClickNext={handleClickNext}
         />
         {pictures.length > 1 && (
-          <p className="gallery__index">{currentIndex + 1}/{pictures.length}</p>
+          <p className="gallery__index">
+            {currentIndex + 1}/{pictures.length}
+          </p>
         )}
       </div>
+
       <div className="detail__content">
-        <div>
-          <h2 className="detail__title">{title}</h2>
-          <p className="detail__location">{location}</p>
-        </div>
-        <div className="detail__host">
-          <div className="host-info">
-            <p className="detail__host--name">{hostFirstName}</p>
-            <p className="detail__host--name">{hostLastName}</p>
+        <div className="detail__content--name">
+          <div className="detail__content--title">{title}</div>
+          <div className="detail__content--location">{location}</div>
+          <div className="detail__content--tags">
+            {tags.map((tag, index) => (
+              <span key={index} className="tag">
+                {tag}
+              </span>
+            ))}
           </div>
-          <img className="host-picture" src={hostPicture} alt="Votre hôte" />
+        </div>
+        <div className="detail__content--infoHost">
+          <div className="detail__content--host">
+            <div className="detail__content--hostInfo">
+              <p className="detail__content--hostName">{hostFirstName}</p>
+              <p className="detail__content--hostName">{hostLastName}</p>
+            </div>
+            <img
+              className="detail__content--hostPicture"
+              src={hostPicture}
+              alt="Votre hôte"
+            />
+          </div>
+          <div className="detail__content--rating">
+            <StarRating score={rating} />
+          </div>
         </div>
       </div>
-      <div className="detail__rating">
-        <StarRating score={rating} />
-      </div>
-      <div className="detail__tags">
-        {tags.map((tag, index) => (
-          <span key={index} className="tag">
-            {tag}
-          </span>
-        ))}
-      </div>
-      <div className="detail__info-container">
+      <div className="detail__infoContainer">
         <Description description={description} />
         <Equipments equipments={equipments} />
       </div>
@@ -75,5 +101,3 @@ function AccomodationDetail() {
 }
 
 export default AccomodationDetail;
-
-
