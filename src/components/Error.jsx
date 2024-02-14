c44146c028c50365c84bb7a43acbb26400531ed8
emarthkopp <emarth@outlook.com>
2024-02-14 13:58:21 +0100

    corrections 2

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,15 +6,18 @@ import '../styles/Error.scss';
 
 function Error() {
     return (
-        <div>
-   
-        <div className='errorPage'>
-            <h2 className="errorTitle">404</h2>
-            <div className="errorText">Oups! la page que vous demandez n'existe pas.</div>
-            <Link to="/" className='linkError'>Retourner sur la page d'accueil </Link>
+      <div>
+        <div className="errorPage">
+          <h2 className="errorTitle">404</h2>
+          <div className="errorText">
+            Oups! la page que vous demandez n'existe pas.
+          </div>
+          <Link to="/" className="linkError">
+            Retourner sur la page d'accueil{" "}
+          </Link>
         </div>
-        </div>
-    )
+      </div>
+    );
 }
  
 export default Error
